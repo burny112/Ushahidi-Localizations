f659920e3678c3b06c6a465c103e6165cdc9d620
burny112 <horst@einsatztraining.de>
2012-01-01 17:33:34 +0100

    Update de_DE/notifications.php

--- de_DE/notifications.php
+++ de_DE/notifications.php
@@ -1,21 +1,21 @@
 <?php
 	$lang = array(
-	'admin_footer' => 'Diese Nachricht wurde von Deiner Webseite geschickt.',
+	'admin_footer' => 'Diese Nachricht wurde von Ihrer Webseite geschickt.',
 	'admin_login_url' => 'Administrator Login',
 	'admin_new_comment' => array(
 		'message' => 'Ein neuer Kommentar wurde eingereicht bezogen auf:',
 		'subject' => 'Neuer Kommentar',
 	),
 	'admin_new_email' => array(
-		'message' => 'Eine neue eMail wurde an Deine Website geschickt.',
+		'message' => 'Eine neue eMail wurde an Ihre Website geschickt.',
 		'subject' => 'Neue eMail.',
 	),
 	'admin_new_report' => array(
-		'message' => 'Ein neuer Bericht wurde auf Deiner Website eingereicht.',
+		'message' => 'Ein neuer Bericht wurde auf Ihrer Website eingereicht.',
 		'subject' => 'Neuer Bericht',
 	),
 	'admin_new_sms' => array(
-		'message' => 'Eine neue Textnachricht wurde an Deine Website geschickt.',
+		'message' => 'Eine neue Textnachricht wurde an Ihre Website geschickt.',
 		'subject' => 'Neue Textnachricht',
 	));
 ?>
